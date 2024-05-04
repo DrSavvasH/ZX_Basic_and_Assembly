ba5f60f96fb101fe9d2701b38d30a4e6dfbc928b
DrSavvasH <151399636+DrSavvasH@users.noreply.github.com>
2024-05-04 17:39:20 +0300

    Update rectangle_classicz80.asm

--- rectangle_classicz80.asm
+++ rectangle_classicz80.asm
@@ -1,100 +1,63 @@
-; draws rectangle on screen using algorithm to calculate PIXELADDRESS (can be used with any classic ZX Spectrum, no PIXELAD/PIXELDN commands as in NEXT)
-org 30000 
-;address should be the same as the one loaded and called from BASIC. However, number of bytes should be exact
-start:
-;border color
-	   LD a,3
-	   OUT ($FE),a ; 254,a
-; attributes (code by Spiteworx https://www.youtube.com/watch?v=jE8ksX8_0No&t=352s)
-	   LD HL,$5800 ; start of attribute memory (SCREEN MEMORY COLOR DATA)
- 	   LD A, 0+0+56+3 ;64+48+2 attributes FLASH *128 + BRIGHT *64 + PAPER *8 + INK
-	   LD (HL),a   ; Set first byte of attribute memory
-; set attributes for the whole screen (Copies first byte to the following 767 bytes)
-	   ; DRAW HORIZONTAL LINE
-	   LD DE, $5801 ; First destination address of LDIR
-	   LD BC, 767 ; 767 number of bytes to copy to display memory (23x32 characters on screen)
-	   LDIR
-; END OF ATTRIBUTE ROUTINE
+; draws rectangle on screen using algorithm to calculate PIXELADDRESS (can be used with any classic ZX Spectrum, no PIXELAD/PIXELDN commands as in Spectrum NEXT
+;
+ORG 30000
+SCREEN  EQU $4000                   ; Location of screen
+COLOR   EQU $5800                   ; Location of color array
+; Get screen address
+;  d = Y pixel position
+;  e = X pixel position
+; GETPIXEL Returns address in HL
+		
+	ld d,0
+	ld e,200
+	call GETPIXEL
 
-DRAW_UPPER_HORIZONTAL_LINE:
-	   LD D,40 ; X (vertical)
-	   LD C,50 ; Y (horizontal)
-	   CALL Get_Pixel_Address
-	   LD B,20 ; line length *8pixel/ rectangle width
-	   LD A,%11111111; 8 pixel graphic line
-	   LD E,8
-LOOP:
-	   LD(HL),A
-	   INC HL
-	   LD A,C ; using A for the addition
-	   ADD A,E
-	   LD C,A
-	   LD A,%11111111; restores function of A
-	   DJNZ LOOP
-	   LD E,7 ; corrects rightmost position of the cursor
-           LD A,C
-	   SUB E
-	   LD C,A
+	ld b,20
+DrawHLine:
+	inc l
+	ld (hl),255
+	djnz DrawHLine
+	
+	ld b,5
+DrawVLine:
+	inc h
+	ld (hl),$00000001
+	djnz DrawVLine
 
-DRAW_RIGHT_VERTICAL_LINE:
-	   LD B,10
-	   LD A,%00000001
-LOOPV1:
-	   INC D
-           CALL Get_Pixel_Address       
-	   LD (HL),A
-	   DJNZ LOOPV1
+	ld b,20
+BackDrawHLine:
+	ld (hl),255
+	dec l
+	djnz BackDrawHLine
+             
+	ld b,5
+BackDrawVLine:
+	ld (hl),$00000001
+	dec h
+	djnz BackDrawVLine
+	ld (hl),$00000001
+	ret
 
-DRAW_LOWER_HORIZONTAL_LINE:
-	   LD B,20 ; line length *8pixel/ rectangle width
-	   LD A,255
-	   LD E,8
-LOOP2:
-	   LD(HL),A
-	   DEC HL
-	   LD A,C ; using A for the addition
-	   SUB E
-	   LD C,A
-	   LD A,%11111111; restores function of A
-	   DJNZ LOOP2
-	   LD A,%00000011
-           LD(HL),A      
-DRAW_LEFT_VERTICAL_LINE:
-	   LD B,10
-	   LD A,%00000001
-LOOPV2:
-	   DEC D
-           CALL Get_Pixel_Address       
-	   LD (HL),A
-	   DJNZ LOOPV2
-
-; returns to BASIC
-	   RET
-
-Get_Pixel_Address:	; Code kindly borrowed (and modified) from http://www.breakintoprogram.co.uk/hardware/computers/zx-spectrum/screen-memory-layout
-			LD A,D				; Calculate Y2,Y1,Y0
-			AND %00000111			; Mask out unwanted bits
-			OR %01000000			; Set base address of screen
-			LD H,A				; Store in H
-			LD A,D				; Calculate Y7,Y6
-			RRA				; Shift to position
-			RRA
-			RRA
-			AND %00011000			; Mask out unwanted bits
-			OR H				; OR with Y2,Y1,Y0
-			LD H,A				; Store in H
-			LD A,D				; Calculate Y5,Y4,Y3
-			RLA				; Shift to position
-			RLA
-			AND %11100000			; Mask out unwanted bits
-			LD L,A				; Store in L
-			LD A,C				; Calculate X4,X3,X2,X1,X0
-			RRA				; Shift into position
-			RRA
-			RRA
-			AND %00011111			; Mask out unwanted bits
-			OR L				; OR with Y5,Y4,Y3
-			LD L,A				; Store in L
-			LD A,C
-			AND %0111			; found that this modification was necessary to the snippet from breakintoprogram
-			RET
+GETPIXEL:   	;code below kindly borrowed from https://wiki.speccy.org/_media/cursos/ensamblador/zx_display.pdf 
+        ld a, e
+        add a, a
+        ld e, a 
+        ; next, rotate the coordinate right three bits (to get to Y3, Y4, Y5 in L)
+        rrca
+        rrca
+        rrca        
+        ; AND any additional bits off
+        and 0xe0
+        ; Add in the x offset twice for 16 pixel step
+        add a, d
+        add a, d        
+        ld l, a  ; coordinate bottom byte done
+        ; next we do the same for Y6 and Y7; no need to shift because we're in the
+        ; right place.
+        ld a, e
+        and 0x18 ; AND extra bits off, and h is done.
+        ld h, a
+        ld de, SCREEN
+        add hl, de  ; now hl points at the screen offset we want
+	ld a,255  ; Get character to print                      ; See if it '$' terminator
+	ret
